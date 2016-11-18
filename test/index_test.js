01bc660ff224a050789e3b437f4003f9e4fb0911
Marty Fitchen <mfitchen@gmail.com>
2016-11-17 16:28:27 -0800

    add a better JSON file

---
+++ test/index_test.js
@@ -0,0 +1,42 @@
+// import { expect } from '../test_helper';
+// import { SAVE_COMMENT } from '../../src/actions/types';
+// import { saveComment } from '../../src/actions';
+//
+// describe('actions', () => {
+//   describe('saveComment', () => {
+//     it('has the correct type', () => {
+//       const action = saveComment();
+//       expect(action.type).to.equal(SAVE_COMMENT);
+//     });
+//
+//     it('has the correct payload', () => {
+//       const action = saveComment('new comment');
+//       expect(action.payload).to.equal('new comment');
+//     });
+//   });
+// });
+
+// describe(‘index page', () => {
+//   describe(‘url name', () => {
+//     it(‘url is the correct url', () => {
+//       var driver = new webdriver.Builder().
+//     withCapabilities(webdriver.Capabilities.chrome()).
+//     build();
+// driver.get(‘http://www.mfitchen.com);
+// var url = driver.currentUrl
+//      expect(url).to.equal(”url name");
+//    });
+//  });
+// });
+
+// test.describe('mfitchen website', function() {
+//   test.it('url should be', function() {
+//     var driver = new webdriver.Builder().
+//   withCapabilities(webdriver.Capabilities.chrome()).
+//   build();
+// driver.get('http://www.mfitchen.com');
+//     var url = driver.currentUrl;
+//        assert.equal(url, 'your_url');
+//     driver.quit();
+//   });
+// });
